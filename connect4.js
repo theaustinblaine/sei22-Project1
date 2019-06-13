38f5c9c1304494cf9fc30d9ffe40534250f84dac
Austin Blaine <theaustinblaine@gmail.com>
2019-06-13 11:15:58 -0400

    Added basic function to adjust class for one player.

--- connect4.js
+++ connect4.js
@@ -23,8 +23,12 @@ function reset() {
     $("#game-board").append('<div id="F' + i + '" class="empty"></div>');
   }
 }
-
-
 reset();
 
+const spaces = document.querySelectorAll('.empty')
+// ...space
 
+spaces.forEach( space => space.addEventListener('click', function(){
+    space.classList.remove('empty')
+    space.classList.add('red')
+}));
