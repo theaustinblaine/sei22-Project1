b8e3f7d7e8a297590d7776750d6132c6af5e2caf
Austin Blaine <theaustinblaine@gmail.com>
2019-06-16 13:31:40 -0400

    added win checks for all directions, unsure if functioning right. logic seems right.

--- connect4.js
+++ connect4.js
@@ -31,24 +31,25 @@ function reset() {
 
   for (let col = 0; col < 7; col++) {
     $("#game-board").append(
-        '<div id="F' + col + '" class="empty" data-column="' + col + '")></div>'
-        );
-    }
+      '<div id="F' + col + '" class="empty" data-column="' + col + '")></div>'
+    );
+  }
 }
 
 reset();
 
 let board = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-]
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
+];
 
-let row = board[i]
-let column = board[i][i]
+// let row = board[i]
+// let column = board[i][parseInt(evt.target.dataset.column)]
+// let column = board[1]
 
 let isPlayer1Turn = true;
 
@@ -60,8 +61,8 @@ document.querySelector("#game-board").addEventListener("click", function(evt) {
     if (column[i].className === "empty") {
       column[i].classList.remove("empty");
       column[i].classList.add(`${isPlayer1Turn ? "black" : "red"}`);
-      board[i][parseInt(evt.target.dataset.column)]= isPlayer1Turn ? "2" : "1"
-      console.log(board)
+      board[i][parseInt(evt.target.dataset.column)] = isPlayer1Turn ? "2" : "1";
+      console.log(board);
       break;
     }
   }
@@ -86,25 +87,76 @@ spaces.forEach(space =>
 
 // Add Point to player score on win (test with each move)
 
-let player1Score = $("#player1 .score").text()
-let player2Score = $("#player2 .score").text()
+let player1Score = $("#player1 .score").text();
+let player2Score = $("#player2 .score").text();
 
 function addPoint() {
   if (isPlayer1Turn === true) {
-      player1Score = parseInt(player1Score) + 1;
-      $("#player1 .score").text(player1Score)
+    player1Score = parseInt(player1Score) + 1;
+    $("#player1 .score").text(player1Score);
   } else {
-      player2Score = parseInt(player2Score) + 1
-      $("#player2 .score").text(player2Score)
+    player2Score = parseInt(player2Score) + 1;
+    $("#player2 .score").text(player2Score);
   }
 }
 
 // Win condition: if 4 in a row, alert 1 person winner and add point to score
+//need a for loop to check each individual solution
 
-// function to check up and down match
-// use the same column, check for consecutive column index
+//if horizontal win, alert for winner, and add a point. Then Reset the board.
+for (let row = 0; row < board.length; row++) {
+  for (let col = 0; col < board[row].length; col++) {
+    if (
+      board[row][col] != `0` &&
+      board[row][col] == board[row][col + 1] &&
+      board[row][col] == board[row][col + 2] &&
+      board[row][col] == board[row][col + 3]
+    ) {
+      console.log("Player Win");
+    }
+  }
+}
 
-// function to check left to right match
-// use same rows, check for ID pattern (A0, A1, A2, A3)
+//ELSE IF vertical win, alert for winner, and add a point. Then Reset the board.
+for (let row = 0; row < board.length; row++) {
+  for (let col = 0; col < board[row].length; col++) {
+    if (
+      board[row][col] != `0` &&
+      board[row][col] == board[row + 1][col] &&
+      board[row][col] == board[row + 2][col] &&
+      board[row][col] == board[row + 3][col]
+    ) {
+      console.log("Player Win");
+    }
+  }
+}
 
+//ELSE diagonal win, alert for winner, and add a point. Then Reset the board.
 
+//one direction
+for (let row = 0; row < board.length; row++) {
+  for (let col = 0; col < board[row].length; col++) {
+    if (
+      board[row][col] != `0` &&
+      board[row][col] == board[row + 1][col + 1] &&
+      board[row][col] == board[row + 2][col + 2] &&
+      board[row][col] == board[row + 3][col + 3]
+    ) {
+      console.log("Player Win");
+    }
+  }
+}
+
+//other direction
+for (let row = 0; row < board.length; row++) {
+  for (let col = 0; col < board[row].length; col++) {
+    if (
+      board[row][col] != `0` &&
+      board[row][col] == board[row + 1][col - 1] &&
+      board[row][col] == board[row + 2][col - 2] &&
+      board[row][col] == board[row + 3][col - 3]
+    ) {
+      console.log("Player Win");
+    }
+  }
+}
